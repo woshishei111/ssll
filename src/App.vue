05050c4c7ca2c9025d3adecf97092f927e2be877
woshishei111 <85855130+woshishei111@users.noreply.github.com>
2022-10-16 15:17:23 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <indexPage></indexPage>
+</template>
+
+<script setup>
+import indexPage from './components/indexPage.vue';
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background-color: rgb(242, 244, 244);
+  color: #2c3e50;
+  min-height: 100vh;
+}
+</style>
